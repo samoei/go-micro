cdd054a3e16870dab716ebe5d0be7862b9344cf1
Phil Samoei <phil@samphiltech.com>
2023-02-06 23:16:58 +0100

    v03

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -15,11 +15,17 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		p.getProducts(resp, req)
+	}
+
+	resp.WriteHeader(http.StatusMethodNotAllowed)
+}
+func (p *Products) getProducts(resp http.ResponseWriter, req *http.Request) {
 	productList := data.GetProducts()
 	err := productList.ToJSON(resp)
 
 	if err != nil {
 		http.Error(resp, "Unable to fetch products", http.StatusInternalServerError)
 	}
-
 }
