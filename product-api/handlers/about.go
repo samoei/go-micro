bf20ad0613f5a7c26a49864a95132511a976501c
Phil Samoei <phil@samphiltech.com>
2023-02-06 19:43:51 +0100

    v02

--- product-api/handlers/about.go
+++ product-api/handlers/about.go
@@ -1,10 +1,30 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 type About struct {
+	l *log.Logger
 }
 
-func (h *About) ServeHTPP(rw http.ResponseWriter, r *http.Request) {
+func NewAbout(l *log.Logger) *About {
+	return &About{l}
+}
+
+func (a *About) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	a.l.Println("Welcome to the About Page")
+
+	data, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(rw, "An Error Occurred", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(rw, "%s", data)
 
 }
--- product-api/main.go
+++ product-api/main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"context"
 	"log"
 	"main/handlers"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	logger := log.New(os.Stdout, "product-api", log.LstdFlags)
+	//handlers do need a logger
+	logger := log.New(os.Stdout, "product-api:", log.LstdFlags)
+
+	//create a home page handler
 	home := handlers.NewHome(logger)
+	about := handlers.NewAbout(logger)
 
-	serverMux := http.NewServeMux()
-	serverMux.Handle("/", home)
+	// Create a server mux
+	handler := http.NewServeMux()
 
-	// listen from all IPs and serve on port 9091
-	http.ListenAndServe(":9091", serverMux)
+	//register handlers
+	handler.Handle("/", home)
+	handler.Handle("/about", about)
+
+	server := http.Server{
+		Addr:         ":9091",
+		Handler:      handler,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	logger.Println("Received a terminate request of type:", sig, "\nGraceful shutdown...")
+
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(timeoutContext)
 }
