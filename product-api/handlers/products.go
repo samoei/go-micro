ff7b8da2ce024b7cb4d4a980faddc84c7787d6f4
Phil Samoei <phil@samphiltech.com>
2023-02-07 23:27:31 +0100

    v04 - Updating product

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"main/data"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -23,6 +25,27 @@ func (p *Products) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
 		p.createProduct(resp, req)
 		return
+	}
+
+	if req.Method == http.MethodPut {
+		r := regexp.MustCompile("/([0-9]+)")
+		group := r.FindAllStringSubmatch(req.URL.Path, -1)
+
+		if len(group) != 1 {
+			http.Error(resp, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		if len(group[0]) != 2 {
+			http.Error(resp, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		idStr := group[0][1]
+		id, _ := strconv.Atoi(idStr)
+
+		p.updateProducts(id, resp, req)
+
 	}
 
 	resp.WriteHeader(http.StatusMethodNotAllowed)
@@ -48,4 +71,21 @@ func (p *Products) createProduct(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Updating product")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(w, "Unable to parse the payload", http.StatusBadRequest)
+		return
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	}
 }
